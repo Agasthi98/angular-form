b0873b67db44c229022e1304404286ea8dc8a02b
Agasthi98 <sankalana98@gmail.com>
2023-11-01 15:46:17 +0530

    retrive records to table

--- src/app/form-component/form-component.component.ts
+++ src/app/form-component/form-component.component.ts
@@ -8,16 +8,39 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class FormComponentComponent {
 
+  submitRecords: any[]
 
+constructor() {
+  this.submitRecords = [];
+}
+  /***
+   * Form group & validations
+   */
   myForm = new FormGroup({
     name: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
-    phone: new FormControl('', [Validators.required, Validators.pattern('[- +()0-9]+')]),
+    phone: new FormControl('', [Validators.required, Validators.pattern('[- +()0-9]+'), Validators.maxLength(10)]),
     amount: new FormControl('', [Validators.required, Validators.min(100)]),
   });
 
+
+  /**
+   * Submit form
+   */
   onSubmit(){
-    console.log(this.myForm.value);
+    const records = this.myForm.value;
+    console.log(this.submitRecords);
+
+    if(this.submitRecords.some((item: any) => item.email === records.email)){
+      alert('Email already exists')
+      return;
+    }else if(this.submitRecords.some((item: any) => item.phone === records.phone)){
+      alert('Phone already exists')
+      return;
+    }
+
+    this.submitRecords.push(records);
+
   }
 
 }
