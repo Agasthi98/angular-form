404bf4b5ad79e3fbbb4973f382dd2767aa3c6a53
Agasthi98 <sankalana98@gmail.com>
2023-11-01 19:09:54 +0530

    sort

--- src/app/form-component/form-component.component.ts
+++ src/app/form-component/form-component.component.ts
@@ -4,50 +4,68 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-form-component',
   templateUrl: './form-component.component.html',
-  styleUrls: ['./form-component.component.scss']
+  styleUrls: ['./form-component.component.scss'],
 })
 export class FormComponentComponent {
-  submitRecords: any[]
+  submitRecords: any[];
+  storedData: any[] = [];
 
-constructor() {
-  this.submitRecords = [];
-}
+  constructor() {
+    this.submitRecords = [];
+  }
   /***
    * Form group & validations
    */
   myForm = new FormGroup({
     name: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
-    phone: new FormControl('', [Validators.required, Validators.pattern('[- +()0-9]+'), Validators.maxLength(10)]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+    ]),
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[- +()0-9]+'),
+      Validators.maxLength(10),
+    ]),
     amount: new FormControl('', [Validators.required, Validators.min(100)]),
   });
-
 
   /**
    * Submit form
    */
-  onSubmit(){
+  onSubmit() {
     const records = this.myForm.value;
-    console.log(this.submitRecords);
 
-    if(this.submitRecords.some((item: any) => item.email === records.email)){
-      alert('Email already exists')
+    if (this.submitRecords.some((item: any) => item.email === records.email)) {
+      alert('Email already exists');
       return;
-    }else if(this.submitRecords.some((item: any) => item.phone === records.phone)){
-      alert('Phone already exists')
+    } else if (
+      this.submitRecords.some((item: any) => item.phone === records.phone)
+    ) {
+      alert('Phone already exists');
       return;
     }
 
+    /**
+     * Push records to array
+     */
     this.submitRecords.push(records);
 
+    /**
+     * sort array by amount
+     */
+
+    this.submitRecords.sort((a, b) => b.amount - a.amount);
+    // Reset the form after submission
+    this.myForm.reset();
   }
 
   /**
    * Delete record
    */
 
-  onDelete(index: number){
-    this.submitRecords.splice(index,1)
+  onDelete(index: number) {
+    this.submitRecords.splice(index, 1);
   }
-
 }
