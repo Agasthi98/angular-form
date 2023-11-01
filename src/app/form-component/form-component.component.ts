70135ab21b88b00d2b2ddd24abb369ece7f47ce2
Agasthi98 <sankalana98@gmail.com>
2023-11-01 15:27:20 +0530

    input form validations

--- src/app/form-component/form-component.component.ts
+++ src/app/form-component/form-component.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-component',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-component.component.scss']
 })
 export class FormComponentComponent {
+
+
+  myForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+    phone: new FormControl('', [Validators.required, Validators.pattern('[- +()0-9]+')]),
+    amount: new FormControl('', [Validators.required, Validators.min(100)]),
+  });
+
+  onSubmit(){
+    console.log(this.myForm.value);
+  }
 
 }
